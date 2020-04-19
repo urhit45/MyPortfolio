5644934a123a2d11d9b7232d336ee02f016ba892
Rohit Upadhyay <rohit.upadhyay@ecs.co.uk>
2020-04-19 22:06:38 +0530

    corrected grammar

--- src/components/about.js
+++ src/components/about.js
@@ -14,7 +14,7 @@ export default class About extends Component {
                                             <span className="heading-meta">About Us</span>
                                             <h2 className="colorlib-heading">Who Am I?</h2>
                                             <p>
-                                                I am a working professional currently working with ECS Infosolutions Pvt. Ltd as a AWS Solutions Architect, currently studying for the Associate Developer. I got my graduation in Computer Science from 
+                                                I am a working professional currently working with ECS Infosolutions Pvt. Ltd as an AWS Solutions Architect, currently studying for the Associate Developer. I got my graduation in Computer Science from 
                                                 DY Patil's Ramrao Adik Institue of Technology, Nerul, Navi Mumbai . Being a computer science grad my expertise lie in related domains of Web Development, DevOps and Data Science. Machine Learning and AI excite me, gets me thinking as I love me a challenge. By best-friends are Stack Overflow, GitHub, reddit and Quora.</p>
                                             <p>
                                                 Now with the boring stuff aside, I write, I sketch also I would love to be able to sing but that's just pushing my luck too much. I am someone who loves to learn, "wait, where else have I heard that?".
